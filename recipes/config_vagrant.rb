aaf8ff4a33829b12ccace7d4fdcfcb329fa7894a
Chris <chris@buryta.com>
2013-02-26 09:00:25 -0500

    add centos host virtualbox build fix's, though still not working on rackspace cloud due to vbox incompatable with xen

--- recipes/config_vagrant.rb
+++ recipes/config_vagrant.rb
@@ -13,11 +13,19 @@ package "VirtualBox-4.2" do
     action :install
 end
 
-# add vagrant rpm
-# yum_repository "vagrant" do
-#     repo_name "vagrant"
-#     url "http://files.vagrantup.com/packages/476b19a9e5f499b5d0b9d4aba5c0b16ebe434311/vagrant_x86_64.rpm"
-#     action :add
-# end
+# CENTOS TESTED ONLY
 
-# install vagrant as a ruby gem
+# note: per http://wiki.centos.org/HowTos/Virtualization/VirtualBox possible that
+# the Dell version is best, keep RPMForge option in mind
+package "dkms"
+execute "yum groupinstall \"Base\" -y"
+execute "yum groupinstall \"Core\" -y"
+execute "yum groupinstall \"Development Tools\" -y"
+package "kernel-devel"
+
+# note: the ls /lib/modules/2.6.32-279.el6.x86_64 -hal showed that there were broken symlinks
+# so the fix to get past that was to set KERN_DIR to the proper path (close to where the symlink
+# was trying to link to but not quite)
+execute "KERN_DIR=/usr/src/kernels/2.6.32-279.22.1.el6.x86_64 /etc/init.d/vboxdrv setup"
+
+# FAIL - the Rackspace cloud is aparantly a XEN kernel environment, which is unsupported by VirtualBox
