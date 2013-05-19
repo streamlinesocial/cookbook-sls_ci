c0fbbaae05b85bd5f437f6cd3b551f52540b8ce5
Chris <chris@buryta.com>
2013-05-19 01:50:47 -0400

    added updates to help node ssh

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+0.2.0
+=====
+
+- added use of data_bag/users/jenkins-node to store pub/private key for that user
+- added private key to nodes that include the sls_ci::node recipe, this enables us to
+  extend the config in the jenksins::node_ssh recipe to ensure these nodes have access
+  to our repos using the same key on each node
+
 0.1.0
 =====
 
--- recipes/node.rb
+++ recipes/node.rb
@@ -2,4 +2,21 @@
 node.set["jenkins"]["node"]["ssh_host"] = node["ipaddress"]
 node.set["jenkins"]["node"]["name"] = node["fqdn"]
 
-# jenkins_cli "login --username #{node[:jenkins][:server][:username]} --password #{node[:jenkins][:server][:password]}" do
+# setup the ssh private key
+jenkins_bag = data_bag_item("users", "jenkins-node")
+
+file "#{node['jenkins']['node']['home']}/.ssh/jenkins-node.rsa" do
+    content jenkins_bag["private_key"]
+    owner node['jenkins']['node']['user']
+    group node['jenkins']['node']['user']
+    mode '0600'
+    action :create
+end
+
+# file "#{node['jenkins']['node']['home']}/.ssh/jenkins-node.rsa.pub" do
+#     content jenkins_bag["public_key"]
+#     owner node['jenkins']['node']['user']
+#     group node['jenkins']['node']['user']
+#     mode '0600'
+#     action :create
+# end
